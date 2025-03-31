ab3c4e8bf9fb88b86e80ccd79db0f857f953d9ac
kim-elijah-sol <kimelijahsol@gmail.com>
2025-03-31 13:56:06 +0000

    tailwind 로 flex 사용

--- apps/react-tailwind/src/App.tsx
+++ apps/react-tailwind/src/App.tsx
@@ -2,7 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className='bg-blue-400 w-[150px] h-[150px] rounded-2xl transition-all'></div>
+    <div className='flex items-center justify-center w-full h-[100vh]'>
+      <div className='bg-blue-400 w-[150px] h-[150px] rounded-2xl transition-all'></div>
+    </div>
   )
 }
 
