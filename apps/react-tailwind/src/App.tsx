928b79401bca35801e1ea55305ee9a98eeecb6e6
kim-elijah-sol <kimelijahsol@gmail.com>
2025-03-31 14:01:24 +0000

    hover 액션 추가

--- apps/react-tailwind/src/App.tsx
+++ apps/react-tailwind/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className='flex-center w-full h-[100vh]'>
-      <div className='bg-blue-400 w-[150px] h-[150px] rounded-2xl transition-all'></div>
+      <div className='bg-blue-400 w-[150px] h-[150px] rounded-2xl transition-all hover:scale-110 cursor-pointer'></div>
     </div>
   )
 }
