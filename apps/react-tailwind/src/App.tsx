c467f4f7ee552d3f2219d35908af43e880c7ac8e
kim-elijah-sol <kimelijahsol@gmail.com>
2025-03-31 14:00:06 +0000

    @utility 활용해서 custom class 만들기

--- apps/react-tailwind/src/App.tsx
+++ apps/react-tailwind/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className='flex items-center justify-center w-full h-[100vh]'>
+    <div className='flex-center w-full h-[100vh]'>
       <div className='bg-blue-400 w-[150px] h-[150px] rounded-2xl transition-all'></div>
     </div>
   )
