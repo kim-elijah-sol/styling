7251e636f5662a5699776c7605e620389875f202
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-01 13:12:07 +0000

    tailwind 로 colour 카드 디자인 만듬 맘쏙 레퍼런스 찾음

--- apps/react-tailwind/src/App.tsx
+++ apps/react-tailwind/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function Wrapper({ children }: PropsWithChildren) {
   return (
-    <main className='w-full p-8 overflow-y-auto gap-2 flex flex-wrap'>
+    <main className='w-full p-8 overflow-y-auto gap-6 flex flex-wrap'>
       {children}
     </main>
   )
@@ -15,14 +15,26 @@ type CardProps = {
 
 function Card({ colors }: CardProps) {
   return (
-    <div className='rounded-[16px] shadow-md flex overflow-hidden'>
-      {colors.map((it) => (
-        <div
-          key={it}
-          className='w-[120px] h-[120px]'
-          style={{ backgroundColor: it }}
-        />
-      ))}
+    <div className='rounded-2xl shadow-lg p-3'>
+      <div className='flex h-[100px] w-[400px] rounded-lg overflow-hidden mb-2'>
+        {colors.map((it) => (
+          <div
+            className='flex-1 cursor-pointer'
+            key={it}
+            style={{ background: it }}
+          />
+        ))}
+      </div>
+      <div className='flex w-[400px] flex justify-around'>
+        {colors.map((it) => (
+          <button
+            key={it}
+            className='px-3 py-1 rounded-md transition-all hover:bg-slate-200 cursor-pointer active:bg-slate-300'
+          >
+            <p className='text-slate-700 font-bold'>{it.replace('#', '')}</p>
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
