28c3d201961b9f7eaed671153960ae86369203b3
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-04 14:07:32 +0000

    stitches util export 해서 쓰기

--- apps/react-stitches/src/App.tsx
+++ apps/react-stitches/src/App.tsx
@@ -1,8 +1,13 @@
-import { Button, WarningButton } from '@styling/stitches'
+import { Button, styled, WarningButton } from '@styling/stitches'
+
+const TitleText = styled('h1', {
+  fontSize: '1rem',
+})
 
 function App() {
   return (
     <div>
+      <TitleText>제목입니당</TitleText>
       <Button type='button'>안녕하세요!</Button>
       <WarningButton type='button'>위험한 버튼이에요!</WarningButton>
     </div>
