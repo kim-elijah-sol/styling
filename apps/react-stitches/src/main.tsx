55380e1f06deba6c28fb0b7bd5392f83e0363d6d
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-04 14:26:13 +0000

    stitches globalStyle 설정

--- apps/react-stitches/src/main.tsx
+++ apps/react-stitches/src/main.tsx
@@ -1,6 +1,9 @@
+import { globalStyle } from '@styling/stitches'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+
+globalStyle()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ packages/stitches/src/globalStyle.ts
@@ -0,0 +1,8 @@
+import { globalCss } from './stitches'
+
+export const globalStyle = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+  },
+})
